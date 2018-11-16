21c9e1305d660b402b2de2db1be38fd59a52ddc4
david.pohlman <david.pohlman@elev.ga.lbs.se>
2018-11-16 15:52:59 +0100

    SkjutGubbe

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    CharacterController cC;
+    public float speed = 5;
+    public float rotationSpeed = 2;
+
+    // Use this for initialization
+    void Start()
+    {
+        cC = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertInput = Input.GetAxis("Vertical");
+        {
+            cC.SimpleMove(transform.forward * vertInput * speed);
+        }
+        float horizInput = Input.GetAxis("Horizontal");
+        {
+            transform.Rotate(Vector3.up * horizInput * rotationSpeed);
+        }
+        
+
+
+    }
+}
---
+++ Assets/Laser.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    LineRenderer lR;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        lR = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lR.enabled = false;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            lR.enabled = true;
+
+            lR.SetPosition(0, transform.position);
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                lR.SetPosition(1, hit.point);
+                hit.collider.GetComponent<Rigidbody>().AddForceAtPosition(transform.forward * 1000 * Time.deltaTime, hit.point);
+            }
+            else
+            {
+                lR.SetPosition(1, transform.forward * 1000);
+            }
+        }
+    }
+}
